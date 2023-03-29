5a9306fc3b644f26779f66f3e943e76c24a84194
Begimai3075 <kurmanbekovabegimaj69@gmail.com>
2023-03-29 17:18:14 +0600

    i did hw cycles

---
+++ main.js
@@ -0,0 +1,91 @@
+//? ЗАДАНИЕ 1
+
+// for(let i = 1; i<=100; i++){
+//     console.log(i)
+// }
+
+//? ЗАДАНИЕ 2
+
+// for( let i = 11; i<=33; i++){
+//     console.log(i)
+// }
+
+//? ЗАДАНИЕ 3
+
+// for (i= 0; i< 100; i++){
+//         if ( i%2 === 0){
+//     console.log(i)
+//         }
+//     }
+
+//? ЗАДАНИЕ 4
+
+// let sum = 0
+// for ( let i = 1; i <= 100; i++){
+// sum = sum + i
+// }console.log(sum)
+
+//? ЗАДАНИЕ 5 
+
+//  const arr = [1,2,3,4,5]
+//   for( let elem of arr){
+//     console.log(elem)
+//   }
+
+//? ЗАДАНИЕ 6 
+
+// let result = 0
+// let arr = [1,2,3,4,5]
+// for( let elem of arr){
+//     result = elem + result
+// } console.log(result)
+
+
+//?  ЗАДАНИЕ 7 
+
+// let arr = [2,5,9,15,0,4]
+
+// for(let i = 0; i < arr.length; i++){
+//     if(arr[i]>3 && arr[i]<10){
+//     console.log(arr[i])
+// }
+// }
+
+//? ЗАДАНИЕ 8 
+
+
+// let arr=[2,-3,4,6,7]
+// let sum=0
+
+// for( let i = 0; i<arr.length; i++){
+//     if (arr[i]>0) {
+//         sum += arr [i]
+//     }
+// }console.log(sum)
+
+//? ЗАДАНИЕ 9 
+
+// let arr = [1,2,5,9,4,13,4,10]
+
+// for (let i=0; i<arr.length; i++){
+//     if (arr[i]=4){
+//         console.log('Верно')
+//     }
+// }
+
+//? ЗАДАНИЕ 10 
+
+// let arr = [1,2,3,4,5,6,7,8,9]
+
+// for(let i = 0; i<=arr.length; i++){
+//     console.log('-' +arr[i])
+// }
+
+//? ЗАДАНИЕ ***
+let str = '5'
+for(let i = 0; i < 5; i++){
+   console.log(str)  
+   str = str + '5'  
+}
+
+
